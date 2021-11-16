f2c55dbc0659054594dd3e8b69fd116750617704
lazkmet <lazkmet@gmail.com>
2021-11-15 21:40:24 -0500

    Ball Motion Scripting
    Added basic ball motion, with speed/angle variation on collision.
    Added tags for paddle type, including  special corner colliders
    Created Ball prefab and Ball Manager

---
+++ Assets/Scripts/BallManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour
+{
+    public float startTime = 2f;
+    public GameObject ballPrefab;
+    public Vector3[] spawnPositions = {Vector3.zero};
+    public Vector3[] spawnDirections = {Vector3.forward}; //LENGTH MUST BE EQUAL TO SPAWNPOSITIONS
+    private GameObject currentBall = null;
+    void Start()
+    {
+        StartCoroutine(SpawnBall(startTime, Random.Range(0, spawnPositions.Length)));
+    }
+    private IEnumerator SpawnBall(float time, int positionIndex) {
+        //Position must be within size of vector array, to avoid out of bounds exception
+        yield return (new WaitForSeconds(time));
+        GameObject newBall = Instantiate(ballPrefab, spawnPositions[positionIndex], Quaternion.identity);
+        newBall.GetComponent<BallMotion>().setDirection(spawnDirections[positionIndex]);
+    }
+}
---
+++ Assets/Scripts/BallMotion.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMotion : MonoBehaviour
+{
+    public float ballSpeed = 5f;
+    public float speedVariation = 0.5f;
+    public float angleVariation = 0.05f;
+    private Vector3 currentDirection = Vector3.zero;
+    private float minSpeed, maxSpeed;
+    private void Start()
+    {
+        minSpeed = ballSpeed * (1 - speedVariation);
+        maxSpeed = ballSpeed * (1 + speedVariation);
+    }
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition += currentDirection * ballSpeed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        GameObject obj = other.gameObject;
+        Vector3 newDirection = currentDirection;
+        if (obj.CompareTag("verticalPaddle")) {
+            newDirection.z = newDirection.z * -1;
+        }
+        else if (obj.CompareTag("horizontalPaddle")) {
+            newDirection.x = newDirection.x * -1;
+        }
+        else if (obj.CompareTag("cornerPaddle")) {
+            newDirection.z = newDirection.z * -1;
+            newDirection.x = newDirection.x * -1;
+        }
+
+        ballSpeed = Random.Range(minSpeed, maxSpeed);
+        newDirection.x += Random.Range(newDirection.x * -angleVariation, newDirection.x * angleVariation);
+        newDirection.z += Random.Range(newDirection.z * -angleVariation, newDirection.z * angleVariation);
+        setDirection(newDirection);
+    }
+    public void setDirection(Vector3 newDirection) {
+        currentDirection = newDirection.normalized;
+    }
+}

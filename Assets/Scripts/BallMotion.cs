f9e06be62331e288b61a620e07c8d143f106d597
lazkmet <lazkmet@gmail.com>
2021-11-17 22:24:30 -0500

    Physics changes / Corner Bounce Improvements
    Also added a dotted line for pretty visuals
    Added 4 spawn points: 0-1 are P1 serve, 2-3 are P2 serve
    Tweaked Materials and Shadows so that lighting changes less across scene

--- Assets/Scripts/BallMotion.cs
+++ Assets/Scripts/BallMotion.cs
@@ -25,26 +25,27 @@ public class BallMotion : MonoBehaviour
     {
         GameObject obj = other.gameObject;
         Vector3 newDirection = currentDirection;
+       
+        if (obj.CompareTag("verticalPaddle"))
+        {
+            newDirection.y = -newDirection.y;
+        }
+        else if (obj.CompareTag("horizontalPaddle"))
+        {
+            newDirection.x = -newDirection.x;
+        }
+        /*else if (obj.CompareTag("cornerPaddle"))
+        {
+            newDirection.y = -newDirection.x;
+            newDirection.x = -newDirection.y;
+        }*/
+        //until we create the goal area
+        else {
+            newDirection = -newDirection;
+        }
+        ballSpeed = Random.Range(minSpeed, maxSpeed);
+        //changing rotation formula: https://matthew-brett.github.io/teaching/rotation_2d.html
         if (collisions++ < 1) {
-            if (obj.CompareTag("verticalPaddle"))
-            {
-                newDirection.y = -newDirection.y;
-            }
-            else if (obj.CompareTag("horizontalPaddle"))
-            {
-                newDirection.x = -newDirection.x;
-            }
-            else if (obj.CompareTag("cornerPaddle"))
-            {
-                newDirection.y = -newDirection.x;
-                newDirection.x = -newDirection.y;
-            }
-            //until we create the goal area
-            else {
-                newDirection = -newDirection;
-            }
-            ballSpeed = Random.Range(minSpeed, maxSpeed);
-            //changing rotation formula: https://matthew-brett.github.io/teaching/rotation_2d.html
             float changeInRadians = Random.Range(-angleVariation, angleVariation) * Mathf.PI / 180;
             newDirection.x = Mathf.Cos(changeInRadians) * newDirection.x - Mathf.Sin(changeInRadians) * newDirection.y;
             newDirection.y = Mathf.Sin(changeInRadians) * newDirection.x + Mathf.Cos(changeInRadians) * newDirection.y;

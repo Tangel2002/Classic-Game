7afff434f7e8cc50f1c8a5fe0fd1736400e517b0
Tangel2002 <tangel@ltu.edu>
2021-11-18 07:50:28 -0500

    error removed
    error when hitting wall

--- Assets/Scripts/particleImpact.cs
+++ Assets/Scripts/particleImpact.cs
@@ -15,23 +15,25 @@ public class particleImpact : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         //Vector3 t;
-        if(collision.rigidbody.transform.position.y == 10)
+        if (collision.rigidbody)
         {
-            rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+            if (collision.rigidbody.transform.position.y == 10)
+            {
+                rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+            }
+            if (collision.rigidbody.transform.position.y == -10)
+            {
+                rotation = Quaternion.Euler(-90.0f, 0.0f, 0.0f);
+            }
+            if (collision.rigidbody.transform.position.x == 10)
+            {
+                rotation = Quaternion.Euler(0.0f, 0.0f, -90.0f);
+            }
+            if (collision.rigidbody.transform.position.x == -10)
+            {
+                rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
+            }
         }
-        if(collision.rigidbody.transform.position.y == -10)
-        {
-            rotation = Quaternion.Euler(-90.0f, 0.0f, 0.0f);
-        }
-        if(collision.rigidbody.transform.position.x == 10)
-        {
-            rotation = Quaternion.Euler(0.0f, 0.0f, -90.0f);
-        }
-        if (collision.rigidbody.transform.position.x == -10)
-        {
-            rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
-        }
-
         ContactPoint contact = collision.contacts[0];
         
         Vector3 pos = contact.point;
